8e3d818672c4fadabb08525dd111bdebffc11f54
Maxnuggets <jericholongabela@gmail.com>
2023-08-17 14:57:16 +0800

    UPDATED files

--- src/main/java/FrontDeskApp.java
+++ src/main/java/FrontDeskApp.java
@@ -1,7 +1,2 @@
-import com.example.application.Main;
-
-public class FrontDeskApp {
-    public static void main (String[] args){
-        Main.main(args);
-    }
+package PACKAGE_NAME;public class FrontDeskApp {
 }
